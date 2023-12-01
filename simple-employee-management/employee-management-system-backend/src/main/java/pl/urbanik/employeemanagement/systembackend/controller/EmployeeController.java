34eff308185b653d14b15769ad2fc81c6aa8ffbd
Kamil Urbanik <kamil.urbanik@miggroup.com>
2023-12-01 09:58:49 +0100

    #0017 - add exception class & optimizing code

--- simple-employee-management/employee-management-system-backend/src/main/java/pl/urbanik/employeemanagement/systembackend/controller/EmployeeController.java
+++ simple-employee-management/employee-management-system-backend/src/main/java/pl/urbanik/employeemanagement/systembackend/controller/EmployeeController.java
@@ -1,12 +1,12 @@
 package pl.urbanik.employeemanagement.systembackend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.urbanik.employeemanagement.systembackend.model.Employee;
 import pl.urbanik.employeemanagement.systembackend.service.EmployeeService;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @CrossOrigin("*")
@@ -26,8 +26,15 @@ public class EmployeeController {
         return employeeService.addNewEmployee(employee);
     }
 
-    @GetMapping
-    public Optional<Employee> getEmployee(@PathVariable long id) {
-        return employeeService.getEmployee(id);
+    @GetMapping("{id}")
+    public ResponseEntity<Employee> getEmployee(@PathVariable long id) {
+        Employee employee = employeeService.getEmployee(id);
+        return ResponseEntity.ok(employee);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable long id, @RequestBody Employee employee) {
+        Employee updatedEmployee = employeeService.updateEmployee(id, employee);
+        return ResponseEntity.ok(updatedEmployee);
     }
 }
